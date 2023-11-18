e87ec8e5812b828f3c494d9f91500b2875601472
João Paulo <jpcchaves@outlook.com>
2023-11-18 14:17:16 -0300

    feat: add password input example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,18 @@
-import { InputComponent } from '../../src/components/input/index';
+import { InputComponent, PasswordInput } from '../../src';
 function App() {
   return (
     <div style={{ marginTop: '20px' }}>
       <InputComponent
+        handleBlur={() => console.log()}
+        inputLabel="Nome"
+        inputIdentifier="name"
+        inputValue="testing input"
+        handleChange={() => console.log()}
+        isRequired
+        hasFloatingLabel
+      />
+
+      <PasswordInput
         handleBlur={() => console.log()}
         inputLabel="Nome"
         inputIdentifier="name"
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { InputComponent } from './components/input';
 import { floatingLabelVariant } from './components/input/style';
+import { PasswordInput } from './components/passwordInput';
 
-export { InputComponent, floatingLabelVariant };
+export { InputComponent, PasswordInput, floatingLabelVariant };
