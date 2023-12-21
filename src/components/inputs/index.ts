af9dbdbcf4f2d11bcb1583cef0ca45bbceb9c6ed
jpcchaves <joao.chaves@liferay.com>
2023-12-21 08:18:58 -0300

    0.1.26

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,10 +1,6 @@
-import { InputComponent } from './components/input'
-import { floatingLabelVariant } from './components/input/style'
-import { PasswordInput } from './components/passwordInput'
+import { InputComponent } from './components/input';
+import { floatingLabelVariant } from './components/input/style';
+import { PasswordInput } from './components/passwordInput';
+import TextareaInput from './components/textareaInput';
 
-
-export {
-    InputComponent,
-    PasswordInput,
-    floatingLabelVariant
-}
+export { InputComponent, PasswordInput, TextareaInput, floatingLabelVariant };
